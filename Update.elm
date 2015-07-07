1a65900e7d29d06e169e613abe351b759bb25c98
Dave Aitken <dave.aitken@gmail.com>
2015-07-07 22:41:06 +0100

    Split update up a bit

--- Update.elm
+++ Update.elm
@@ -1,26 +1,37 @@
 module Update where
 
-import Game exposing (Game)
+import Game exposing (Game, Coords)
 import Input exposing (Input)
 
-update : Input -> Game -> Game
-update { dt, thrust, torque, window } g = let
-    window' = { x = toFloat (fst window), y = toFloat (snd window) }
-    t' = g.t + dt
-    a' = case torque of
+updatedAngle : Input -> Game -> Float
+updatedAngle { dt, torque } g = case torque of
             Input.CW -> g.player.angle - g.player.ship.side_thruster_power * dt
             Input.CCW -> g.player.angle + g.player.ship.side_thruster_power * dt
             Input.None -> g.player.angle
-    vx = if thrust then g.player.vel.x + (cos g.player.angle) * g.player.ship.rear_thruster_power * dt else g.player.vel.x
-    vy = if thrust then g.player.vel.y + (sin g.player.angle) * g.player.ship.rear_thruster_power * dt else g.player.vel.y
-    sx = g.player.pos.x + vx * dt
-    sy = g.player.pos.y + vy * dt
-    vlen = sqrt (vx * vx + vy * vy)
-    vscaled = min vlen g.player.ship.max_vel
-    vel' = if vlen > 0 then { x = vx * vscaled / vlen, y = vy * vscaled / vlen } else { x = 0, y = 0 }
-  in { g | t <- t', window <- window', player <- {
-        ship = g.player.ship,
-        pos = { x = sx, y = sy },
-        vel = vel',
-        angle = a'
-      }}
+
+updatedVel : Input -> Game -> Coords
+updatedVel { dt, thrust } g = let 
+        vx = if thrust then g.player.vel.x + (cos g.player.angle) * g.player.ship.rear_thruster_power * dt else g.player.vel.x
+        vy = if thrust then g.player.vel.y + (sin g.player.angle) * g.player.ship.rear_thruster_power * dt else g.player.vel.y
+        vlen = sqrt (vx * vx + vy * vy)
+        vscaled = min vlen g.player.ship.max_vel
+    in if vlen > 0 then { x = vx * vscaled / vlen, y = vy * vscaled / vlen } else { x = 0, y = 0 }
+
+updatedPos : Input -> Game -> Coords
+updatedPos { dt } g = {
+        x = g.player.pos.x + g.player.vel.x * dt,
+        y = g.player.pos.y + g.player.vel.y * dt
+    }
+
+update : Input -> Game -> Game
+update i g = {
+    g |
+        window <- { x = toFloat (fst i.window), y = toFloat (snd i.window) },
+        t <- g.t + i.dt,
+        player <- {
+            ship = g.player.ship,
+            pos = updatedPos i g,
+            vel = updatedVel i g,
+            angle = updatedAngle i g
+            }
+        }
